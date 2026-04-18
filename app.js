679346a0fb4ac62a530e2d2cdd358652baac0d60
Alaaeddine <alaaeddine958@gmail.com>
2026-04-18 17:09:46 +0100

    fix pipeline

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 const FILE = path.join(__dirname, "visits.json");
-
+//test
 // Mutex simple
 let lock = false;
 
